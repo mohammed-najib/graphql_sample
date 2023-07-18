e8d4a42cbb32dfa3edd0ef5dcbafcf59484e4022
Mohammed Najib Yahya <mohammed.njeeb@gmail.com>
2023-07-19 00:21:13 +0300

    added query by id, added nested tables

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,38 @@ const resolvers = {
     games: () => {
       return db.games
     },
+    game: (_: any, args: { id: string }) => {
+      return db.games.find((game) => game.id === args.id)
+    },
     authors: () => {
       return db.authors
     },
+    author: (_: any, args: { id: string }) => {
+      return db.authors.find((author) => author.id === args.id)
+    },
     reviews: () => {
       return db.reviews
+    },
+    review: (_: any, args: { id: string }) => {
+      return db.reviews.find((review) => review.id === args.id)
+    },
+  },
+  Game: {
+    reviews: (parent: { id: string }) => {
+      return db.reviews.filter((review) => review.game_id === parent.id)
+    },
+  },
+  Author: {
+    reviews: (parent: { id: string }) => {
+      return db.reviews.filter((review) => review.author_id === parent.id)
+    },
+  },
+  Review: {
+    game: (parent: { game_id: string }) => {
+      return db.games.find((game) => game.id === parent.game_id)
+    },
+    author: (parent: { author_id: string }) => {
+      return db.authors.find((author) => author.id === parent.author_id)
     },
   },
 }
